9133940460f1d2b010e2a3b489ebb6a19c6aa382
GopersStupid <mikomeysa14@gmail.com>
2022-07-03 09:08:09 +0700

    note : add logo

--- src/component/element/navbar/component/left-component.jsx
+++ src/component/element/navbar/component/left-component.jsx
@@ -1,10 +1,11 @@
 import { Fragment } from "react"
+const logo = require('../../../image/logo512.png')
 
 const LeftComponent = function() {
     return (
         <Fragment>
             <div id="section-left" className="h-full w-full lg:w-[20%] flex items-center ml-3 lg:ml-0">
-                <div id="logo" className=" h-[50px] w-[50px] lg:h-[55px] lg:w-[55px] bg-cover mr-3" style={{backgroundImage: `url(https://raw.githubusercontent.com/Meysadesu/cdn-for-smkbaiti/main/logo512.png?token=GHSAT0AAAAAABWCJTTG5HTK5X33ZFO7EWPAYWAJQGA)`}}>
+                <div id="logo" className=" h-[40px] w-[40px] lg:h-[55px] lg:w-[55px] bg-cover mr-3" style={{ backgroundImage: `url(${logo})` }}>
                 </div>
                 <div id="title-logo" className="title-school font-poppins lg:ml-[10px]">
                     <p>SMK</p>
